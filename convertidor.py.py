1cb45acf071fa468b4177bfe2fdd4e82c6180a51
Guillermomartinez03 <71438040+Guillermomartinez03@users.noreply.github.com>
2020-10-26 13:49:22 +0100

    Add files via upload

---
+++ convertidor.py.py
@@ -0,0 +1,11 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def convertidor():
+	dolar=input("Introduce cantidad en dólares: ")
+	print dolar, "dólares, son: ", dolar*0.84, "euros."
+
+	
+>>> convertidor()
+Introduce cantidad en dólares: 2
+2 dólares, son:  1.68 euros.
+>>> 
---
+++ semana.py
@@ -0,0 +1,8 @@
+# -*- coding: cp1252 -*-
+def semanita():
+    numero = input("Dime un numero del 1 al 7:")
+    
+    semana = ["Lunes", "Martes","Miércoles","Jueves","Viernes","sabado","Domingo"]
+    print semana[numero - 1]
+    
+semanita()
