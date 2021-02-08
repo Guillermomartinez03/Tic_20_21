8ef15712a0304228395bc5607a4f54246af2c30a
Guillermomartinez03 <71438040+Guillermomartinez03@users.noreply.github.com>
2021-02-08 13:51:38 +0100

    Add files via upload

---
+++ Punteros1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int x=2;
+	int y=3;
+	int z=5;
+	int *px;
+	int *py;
+	int *pz; //homoheterobitranssexual noviahelicopterotactico, es una novia, un ghelicoptero y encima tactico
+	px=&x;
+	py=&y;
+	pz=&z;
+	printf("\n x= %d, direccion %X",x,&x);
+	printf("\n y= %d, direccion %X",y,&y);
+	printf("\n z= %d, direccion %X",z,&z);
+	printf("\n x+y+z= %d", *px+*py+*pz);
+	return 0;
+}
